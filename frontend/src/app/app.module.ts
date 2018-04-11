ea5d453d17e6889a39daadc4a3eeb4064662773d
bayendemba <bayendemba@gmail.com>
2018-04-11 12:34:22 +0000

    Angular Routing

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -1,15 +1,14 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { BrowserModule } from "@angular/platform-browser";
+import { NgModule } from "@angular/core";
 
-
-import { AppComponent } from './app.component';
-import { NavbarComponent } from './components/navbar/navbar.component';
-import { LoginComponent } from './components/login/login.component';
-import { SignupComponent } from './components/signup/signup.component';
-import { ProfileComponent } from './components/profile/profile.component';
-import { RequestResetComponent } from './components/password/request-reset/request-reset.component';
-import { ResponseResetComponent } from './components/password/response-reset/response-reset.component';
-
+import { AppComponent } from "./app.component";
+import { NavbarComponent } from "./components/navbar/navbar.component";
+import { LoginComponent } from "./components/login/login.component";
+import { SignupComponent } from "./components/signup/signup.component";
+import { ProfileComponent } from "./components/profile/profile.component";
+import { RequestResetComponent } from "./components/password/request-reset/request-reset.component";
+import { ResponseResetComponent } from "./components/password/response-reset/response-reset.component";
+import { AppRoutingModule } from ".//app-routing.module";
 
 @NgModule({
   declarations: [
@@ -21,10 +20,8 @@ import { ResponseResetComponent } from './components/password/response-reset/res
     RequestResetComponent,
     ResponseResetComponent
   ],
-  imports: [
-    BrowserModule
-  ],
+  imports: [BrowserModule, AppRoutingModule],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
