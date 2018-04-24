703550c510087c9069281608075a78054fd615fe
bayendemba <bayendemba@gmail.com>
2018-04-24 11:20:38 +0000

    Create login

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -1,5 +1,7 @@
 import { BrowserModule } from "@angular/platform-browser";
 import { NgModule } from "@angular/core";
+import { FormsModule } from "@angular/forms";
+import { HttpClientModule } from "@angular/common/http";
 
 import { AppComponent } from "./app.component";
 import { NavbarComponent } from "./components/navbar/navbar.component";
@@ -20,7 +22,7 @@ import { AppRoutingModule } from ".//app-routing.module";
     RequestResetComponent,
     ResponseResetComponent
   ],
-  imports: [BrowserModule, AppRoutingModule],
+  imports: [BrowserModule, AppRoutingModule, FormsModule, HttpClientModule],
   providers: [],
   bootstrap: [AppComponent]
 })
--- frontend/src/app/components/login/login.component.ts
+++ frontend/src/app/components/login/login.component.ts
@@ -1,15 +1,22 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
+import { HttpClient } from "@angular/common/http";
 
 @Component({
-  selector: 'app-login',
-  templateUrl: './login.component.html',
-  styleUrls: ['./login.component.css']
+  selector: "app-login",
+  templateUrl: "./login.component.html",
+  styleUrls: ["./login.component.css"]
 })
 export class LoginComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit() {
+  public form = {
+    email: null,
+    password: null
+  };
+  constructor(private http: HttpClient) {}
+  onSubmit() {
+    // console.log(this.form);
+    return this.http
+      .post("http://localhost:8000/api/auth/login", this.form)
+      .subscribe(data => console.log(data), error => console.log(error));
   }
-
+  ngOnInit() {}
 }
