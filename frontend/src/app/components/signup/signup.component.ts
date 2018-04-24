2d10a03f6e74feec657c6bef3eb07e8ba324e53d
bayendemba <bayendemba@gmail.com>
2018-04-24 15:03:55 +0000

    SignUp

--- frontend/src/app/components/signup/signup.component.ts
+++ frontend/src/app/components/signup/signup.component.ts
@@ -1,15 +1,30 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
+import { HttpClient } from "@angular/common/http";
 
 @Component({
-  selector: 'app-signup',
-  templateUrl: './signup.component.html',
-  styleUrls: ['./signup.component.css']
+  selector: "app-signup",
+  templateUrl: "./signup.component.html",
+  styleUrls: ["./signup.component.css"]
 })
 export class SignupComponent implements OnInit {
+  public form = {
+    email: null,
+    name: null,
+    password: null,
+    password_confirmation: null
+  };
+  public error = [];
 
-  constructor() { }
-
-  ngOnInit() {
+  onSubmit() {
+    return this.http
+      .post("http://localhost:8000/api/signup", this.form)
+      .subscribe(data => console.log(data), error => this.handleError(error));
   }
 
+  handleError(error) {
+    this.error = error.error.errors;
+  }
+  constructor(private http: HttpClient) {}
+
+  ngOnInit() {}
 }
